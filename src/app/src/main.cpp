f3f34368295545f5e636a3857cdf3b21f9eb7e7b
mingtiancai <2427373908@qq.com>
2022-06-16 02:14:01 +0800

    ENH: implement Logger

--- src/app/src/main.cpp
+++ src/app/src/main.cpp
@@ -1,12 +1,11 @@
+#include <string>
+
 #include "mainwindow.h"
 #include <QApplication>
 #include "logger.h"
 
 int main(int argc, char *argv[])
 {
-    core::Logger log;
-    log.run();
-
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
