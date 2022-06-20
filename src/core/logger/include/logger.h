9f19ff970cb1a37b17c5ed1d5dc8f912bfe0a68d
Lingxi Li <lingxl@microsoft.com>
2022-06-20 21:23:17 +0800

    STYLE: improve 'logger' design

--- src/core/logger/include/logger.h
+++ src/core/logger/include/logger.h
@@ -1,46 +1,25 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
-#include <memory>
-#include <map>
+#include <string>
+
 #include <spdlog/logger.h>
 
-#define SPDLOG_BASE(logger, level, ...) (logger)->log(spdlog::source_loc{__FILE__, __LINE__, __func__}, level, __VA_ARGS__)
-#define TRACELOG(...) SPDLOG_BASE(core::Logger::getInstance()->logger(), spdlog::level::trace, __VA_ARGS__)
-#define DEBUGLOG(...) SPDLOG_BASE(core::Logger::getInstance()->logger(), spdlog::level::debug, __VA_ARGS__)
-#define INFOLOG(...) SPDLOG_BASE(core::Logger::getInstance()->logger(), spdlog::level::info, __VA_ARGS__)
-#define WARNLOG(...) SPDLOG_BASE(core::Logger::getInstance()->logger(), spdlog::level::warn, __VA_ARGS__)
-#define ERRORLOG(...) SPDLOG_BASE(core::Logger::getInstance()->logger(), spdlog::level::err, __VA_ARGS__)
-#define CRITICALLOG(...) SPDLOG_BASE(core::Logger::getInstance()->logger(), spdlog::level::critical, __VA_ARGS__)
+#define SPDLOG_BASE(logger, level, ...) (logger).log(::spdlog::source_loc{__FILE__, __LINE__, __func__}, level, __VA_ARGS__)
+#define TRACELOG(...)    SPDLOG_BASE(::core::logger::get(), ::spdlog::level::trace, __VA_ARGS__)
+#define DEBUGLOG(...)    SPDLOG_BASE(::core::logger::get(), ::spdlog::level::debug, __VA_ARGS__)
+#define INFOLOG(...)     SPDLOG_BASE(::core::logger::get(), ::spdlog::level::info, __VA_ARGS__)
+#define WARNLOG(...)     SPDLOG_BASE(::core::logger::get(), ::spdlog::level::warn, __VA_ARGS__)
+#define ERRORLOG(...)    SPDLOG_BASE(::core::logger::get(), ::spdlog::level::err, __VA_ARGS__)
+#define CRITICALLOG(...) SPDLOG_BASE(::core::logger::get(), ::spdlog::level::critical, __VA_ARGS__)
 
-namespace core
-{
+namespace core {
+namespace logger {
 
-    class Logger
-    {
-    public:
-        Logger();
-
-        static Logger *getInstance()
-        {
-            static Logger instance;
-            return &instance;
-        }
-
-        Logger(const Logger &) = delete;
-        Logger &operator=(const Logger &) = delete;
-
-        auto logger()
-        {
-            return logger_;
-        }
-
-        void setLoggerLevel(std::string level);
-
-    private:
-        std::shared_ptr<spdlog::logger> logger_;
-        std::map<std::string, spdlog::level::level_enum> loggerLevelMap_;
-    };
-}
+    spdlog::logger &get();
+    void setLevel(const std::string &level);
+    
+} // namespace logger
+} // namespace core
 
 #endif
--- src/core/logger/src/logger.cpp
+++ src/core/logger/src/logger.cpp
@@ -1,46 +1,59 @@
-#include <iostream>
+#include "logger.h"
+
 #include <chrono>
+#include <format>
+#include <iostream>
+#include <unordered_map>
 
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/daily_file_sink.h"
 
-#include "logger.h"
-
-namespace core
-{
-    Logger::Logger()
+namespace core {
+namespace logger {
+namespace {
+    std::shared_ptr<spdlog::logger> construct()
     {
-        loggerLevelMap_ = {
-            {"trance", spdlog::level::trace},
-            {"debug", spdlog::level::debug},
-            {"info", spdlog::level::info},
-            {"warn", spdlog::level::warn},
-            {"error", spdlog::level::err},
-            {"critical", spdlog::level::critical}};
-
+        using namespace std::chrono;
         try
         {
-            std::chrono::seconds currentTime = std::chrono::duration_cast<std::chrono::seconds>(
-                std::chrono::system_clock::now().time_since_epoch());
-
-            std::string logFile = std::string("logs/") + std::to_string(currentTime.count()) + std::string(".log");
-
-            logger_ = spdlog::daily_logger_mt("codeSource", logFile, 0, 0);
-            logger_->set_pattern("[%D %H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
-            logger_->set_level(spdlog::level::trace);
+            auto currentTime = duration_cast<seconds>(system_clock::now().time_since_epoch());
+            auto logFile = std::format("logs/{}.log", currentTime.count());
+            auto logger = spdlog::daily_logger_mt("codeSource", logFile, 0, 0);
+            logger->set_pattern("[%D %H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
+            logger->set_level(spdlog::level::trace);
+            return logger;
         }
         catch (const spdlog::spdlog_ex &ex)
         {
-            std::cout << "Log init failed: " << ex.what() << std::endl;
-            exit(1);
+            std::cerr << "Log init failed: " << ex.what() << std::endl;
+            throw;
         }
     }
-    void Logger::setLoggerLevel(std::string level)
+} // unnamed namespace
+
+    spdlog::logger &get()
     {
-        if (loggerLevelMap_.find(level) != loggerLevelMap_.end())
+        static std::shared_ptr<spdlog::logger> ptr = construct();
+        return *ptr;
+    }
+
+    void setLevel(const std::string &level)
+    {
+        static const std::unordered_map<std::string, spdlog::level::level_enum> map = {
+            { "trace", spdlog::level::trace },
+            { "debug", spdlog::level::debug },
+            { "info", spdlog::level::info },
+            { "warn", spdlog::level::warn },
+            { "error", spdlog::level::err },
+            { "critical", spdlog::level::critical }
+        };
+        auto it = map.find(level);
+        if (it != map.end())
         {
-            logger_->set_level(loggerLevelMap_[level]);
+            get().set_level(it->second);
             INFOLOG("change logger level to {}", level);
         }
     }
-}
+
+} // namespace logger
+} // namespace core
